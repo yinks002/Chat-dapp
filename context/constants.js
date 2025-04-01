6427fa4a2dc7b05173721494ef9f236f9289e412
Olaiya Olayimika <64112251+yinks002@users.noreply.github.com>
2025-04-01 16:45:37 +0100

    Add files via upload

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,9 @@
 import chatJson from "./ChatApp.json"
-export const ChatAppAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const ChatAppAddress = "0x9dC45FfE9cD1fbC8518bfDE3B326043F757e4FD0";
 
 export const ChatAppABI = chatJson.abi;
+
+
+
+//hardhat localhost
+// 0x5fbdb2315678afecb367f032d93f642f64180aa3
